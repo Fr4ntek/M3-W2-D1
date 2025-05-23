9f9f71dbcbd1effe8430a0d5c6d8a7048d099225
Fr4ntek <francescol9m6@gmail.com>
2025-05-23 19:16:29 +0200

    piccoli change su sprites

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
+    private GameObject[] players;
+
+    private void Start()
+    {
+        players = GetComponentsInChildren<GameObject>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
